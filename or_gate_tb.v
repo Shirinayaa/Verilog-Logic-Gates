4f7130ce28b410037ab77e5c5168d23315078d60
Shirinayaa <shiriyanathan@gmail.com>
2026-05-30 10:52:03 +0530

    Create or_gate_tb.v

---
+++ or_gate_tb.v
@@ -0,0 +1,42 @@
+module or_gate_tb;
+reg A;
+reg B;
+reg expected;
+wire Y;
+
+or_gate uut(
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $display("A B | Y");
+    A=0; B=0; 
+    expected= 0|0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+    if(Y != expected)
+        $display("ERROR");
+    A=0; B=1; 
+    expected= 0|1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+    if(Y != expected)
+        $display("ERROR");
+    A=1; B=0; 
+    expected= 1|0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+    if(Y != expected)
+        $display("ERROR");
+    A=1; B=1; 
+    expected= 1|1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+    if(Y != expected)
+        $display("ERROR");
+    $finish;
+end
+endmodule
+
