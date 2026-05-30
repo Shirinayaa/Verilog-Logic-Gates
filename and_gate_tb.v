d5808b6c1816a83c9eae997339de6c2a6c9e40a8
Shirinayaa <shiriyanathan@gmail.com>
2026-05-30 10:33:20 +0530

    Create and_gate_tb.v

---
+++ and_gate_tb.v
@@ -0,0 +1,32 @@
+module and_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+// Instantiate the AND gate
+and_gate uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $display("A B | Y");
+
+    A = 0; B = 0; #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 0; B = 1; #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 1; B = 0; #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 1; B = 1; #10;
+    $display("%b %b | %b", A, B, Y);
+
+    $finish;
+end
+
+endmodule
