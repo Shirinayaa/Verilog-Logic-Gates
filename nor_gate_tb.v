940b90093042259fa06506bc16238c6c124af200
Shirinayaa <shiriyanathan@gmail.com>
2026-05-30 13:14:43 +0530

    Create nor_gate_tb.v

---
+++ nor_gate_tb.v
@@ -0,0 +1,35 @@
+module nor_gate_tb;
+reg A;
+reg B;
+reg expected;
+wire Y;
+
+nor_gate uut(
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $dumpfile("nor_wave.vcd");
+    $dumpvars(0,nor_gate_tb);
+    $display("A B | Y");
+    A=0; B=0; expected= ~(A|B); #10;
+    $display("%b %b | %b",A,B,Y);
+    if(Y != expected)
+        $display("Error");
+    A=0; B=1; expected= ~(A|B); #10;
+    $display("%b %b | %b",A,B,Y);
+    if(Y != expected)
+        $display("Error");
+    A=1; B=0; expected= ~(A|B); #10;
+    $display("%b %b | %b",A,B,Y);
+    if(Y != expected)
+        $display("Error");
+    A=1; B=1; expected= ~(A|B); #10;
+    $display("%b %b | %b",A,B,Y);
+    if(Y != expected)
+        $display("Error");
+    $finish;
+end
+endmodule
